c1f58f59efdcb3e435cd160a92e9b184fbb9a3db
Pedro <pfcabreira@stefanini.com>
2022-04-25 11:40:04 -0300

    Logger

--- stats.js
+++ stats.js
@@ -1,7 +1,18 @@
 const os = require('os')
+const log = require('./logger')
 
 const { freemem, totalmem } = os
 
+const total = parseInt(totalmem() / 1024 / 1024)
+const mem = parseInt(freemem() / 1024 / 1024)
+const percents = parseInt((mem / total) * 100)
+
+const stats = {
+    free: `${mem} MB`,
+    total: `${total} MB`,
+    usage: `${percents} MB`,
+}
+
 setInterval(() => {
-    console.log(freemem(), totalmem())
+    log(`${JSON.stringify(stats)}\n`)
 }, 2000);
